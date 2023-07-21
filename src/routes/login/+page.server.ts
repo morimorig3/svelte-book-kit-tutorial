c202680f1ff9dee5951809cccbd17dcb1e58615b
morimorig3 <mori.nto@outlook.jp>
2023-07-21 15:36:19 +0900

    feat:TS化 /login

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,19 +1,19 @@
 import { sendPasswordlessLink } from "$lib/server/auth0";
 import { fail } from "@sveltejs/kit";
+import type { Actions } from "./$types";
+import { FORM_ERRORS_LOGIN } from "./const";
 
-export const actions = {
+export const actions: Actions = {
   default: async ({ cookies, request, url }) => {
     const data = await request.formData();
-    const email = data.get("email");
-
-    console.log(`${url.origin}/api/auth/callback`);
+    const email = (data.get("email") as string) || null; // TODO: better typed
 
     if (!email) {
-      return fail(400, { email, error: "missing" });
+      return fail(400, { email, error: FORM_ERRORS_LOGIN.MISSING });
     }
 
     if (!/^.+@.+$/.test(email)) {
-      return fail(400, { email, error: "invalid_format" });
+      return fail(400, { email, error: FORM_ERRORS_LOGIN.INVALID_FORMAT });
     }
 
     const state = crypto.randomUUID();
---
+++ src/routes/login/const.ts
@@ -0,0 +1,8 @@
+export const FORM_NAMES_LOGIN = {
+  EMAIL: "email",
+};
+
+export const FORM_ERRORS_LOGIN = {
+  MISSING: "missing",
+  INVALID_FORMAT: "invalid_format",
+};
